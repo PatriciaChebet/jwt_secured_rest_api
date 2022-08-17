5b5a5165d81de01cc813526679b3e7e0bfff9d1a
PatriciaChebet <patriciacchebet@gmail.com>
2022-08-17 16:08:41 +0300

    Hashes user's password

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,16 @@
 const VerifyUserMiddleware = require("./middlewares/verify.user.middleware");
+const existingUsers = require('../db')
 
 exports.authConfig = function (app) {
 
    app.post('/auth/signup', [
       VerifyUserMiddleware.hasAuthValidFields,
-      VerifyUserMiddleware.checksEmailExists
+      VerifyUserMiddleware.checksEmailExists,
+      VerifyUserMiddleware.hashPassword
    ]);
+
+   app.get('/auth/all', (req, res) => {
+      res.json(existingUsers.users)
+   });
 
 };
